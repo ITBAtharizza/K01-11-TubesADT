1af1faf1bfb911dc5316272ec37f2728e8ab173d
ITBAtharizza <18223079@std.stei.itb.ac.id>
2024-11-19 01:24:22 +0700

    Add MESINKAR and MESINKATA

---
+++ src/ADT/mesinkarakter.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "mesinkarakter.h"
+
+char currentChar;
+boolean EOP;
+static FILE *pita;
+static int retval;
+
+void START(char *filename) {
+    /* Mesin siap dioperasikan. Pita disiapkan ... */
+    /* Algoritma */
+    if (filename == NULL){
+        pita = stdin;
+    }
+    else{
+        pita = fopen(filename, "r");
+    }
+    ADV();
+}
+
+void ADV() {
+    /* Pita dimajukan satu karakter. ... */
+    /* Algoritma */
+    retval = fscanf(pita, "%c", &currentChar);
+    EOP = (currentChar == MARK);
+    if (EOP) {
+        fclose(pita);
+    }
+}
+
+char GetCC() {
+    /* Mengirimkan currentChar */
+    /* Algoritma */
+    return currentChar;
+}
+
+boolean IsEOP() {
+    /* Mengirimkan true jika currentChar = MARK */
+    /* Algoritma */
+    return EOP;
+}

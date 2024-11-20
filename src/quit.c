4619a3a5ab9f1d720204478beadee925a6177b14
Rakditya <adhityaraka10@gmail.com>
2024-11-20 17:59:20 +0700

    feat: Add quit.h

--- src/quit.c
+++ src/quit.c
@@ -1,10 +1,12 @@
+#include "quit.h"
+
 void quit() {
     char pilihan;
     printf("Apakah kamu ingin menyimpan data sesi sekarang (Y/N)? ");
     scanf("%c", &pilihan);
 
     if (pilihan == 'Y' || pilihan == 'y') {
-        // Nanti buat implementasinya
+        // Implementasinya belum dibuat
         printf("Data sesi telah disimpan.\n");
     }
     printf("Kamu keluar dari PURRMART.\nDadah ^_^/\n");
---
+++ src/quit.h
@@ -0,0 +1,10 @@
+#ifndef __QUIT_H__
+#define __QUIT_H__
+
+void quit();
+/*
+Fungsi Quit digunakan untuk menangani pengguna keluar dari toko
+I.S Pengguna sudah login
+F.S Pengguna keluar dari toko dan data dari pengguna disimpan kedalam database
+*/
+#endif

7c6d67c802daa0bb47f9dbcdcaf35f76c9b84109
Rakditya <adhityaraka10@gmail.com>
2024-11-22 21:41:54 +0700

    feat: Complete Quit

--- src/quit.c
+++ src/quit.c
@@ -1,13 +1,18 @@
 #include "quit.h"
+#include "save.h"
 
-void quit() {
+void quit(List *list_user, ListDin *list_barang) {
     char pilihan;
     printf("Apakah kamu ingin menyimpan data sesi sekarang (Y/N)? ");
     scanf("%c", &pilihan);
 
     if (pilihan == 'Y' || pilihan == 'y') {
-        // Implementasinya belum dibuat
-        printf("Data sesi telah disimpan.\n");
+        char filename[55];
+        printf("Masukkan nama file: ");
+        scanf("%s", filename);
+
+        save(filename, list_user, list_barang);
+        printf("Data sesi telah disimpan ke %s.\n", filename);
     }
     printf("Kamu keluar dari PURRMART.\nDadah ^_^/\n");
 }

ce17c7e4684da233db0417ef96c5e30ad71ae356
michelle07-09 <18223037@std.stei.itb.ac.id>
2024-12-18 15:30:37 +0700

    Create riwayat_maksimal.h

---
+++ src/riwayat_maksimal.h
@@ -0,0 +1,14 @@
+#ifndef RIWAYAT_MAKSIMAL_H
+#define RIWAYAT_MAKSIMAL_H
+
+#include <stdbool.h>
+
+/**
+ * @brief Menampilkan riwayat pembelian dengan total maksimal.
+ *
+ * @param history Array yang berisi riwayat pembelian.
+ * @param count Jumlah elemen dalam riwayat.
+ */
+void DisplayMaxPurchase(char history[][100], int count);
+
+#endif // RIWAYAT_MAKSIMAL_H

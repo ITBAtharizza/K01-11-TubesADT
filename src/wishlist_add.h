d7b7baa95ad07a4867500ab586d41770f9308746
daffathall <165241712+daffathall@users.noreply.github.com>
2024-12-18 16:23:51 +0700

    feat wishlist_add.h

--- src/wishlist_add.h
+++ src/wishlist_add.h
@@ -1,17 +1,17 @@
 #ifndef __WISHLIST_ADD_H__
 #define __WISHLIST_ADD_H__
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "boolean.h"
-#include "ADT/mesinkarakter/mesinkarakter.h"
-#include "ADT/mesinkata/mesinkata.h"
-#include "ADT/list/list.h"
-#include "ADT/listdin/listdin.h"
-#include "ADT/queue/queue.h"
-#include "ADT/map/map.h"
-#include "ADT/custom/custom.h"
-#include "game/tebakAngka/tebakAngka.h"
-#include "game/wordl3/wordl3.h"
-#include "game/QuantumWordl3/QuantumWordl3.h"
+#include "list.h"
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+#include "listdin.h"
+#include "custom.h"
+#include "linkedlist.h"
+#include "queue.h"
+#include "map.h"
 
 void WishlistAdd(List *L, ListDin arr);
 

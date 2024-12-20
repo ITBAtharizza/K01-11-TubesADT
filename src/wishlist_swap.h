969e161b89fc85a83deab8fe699d5adc4f4baf12
daffathall <165241712+daffathall@users.noreply.github.com>
2024-12-20 13:13:18 +0700

    Feat wishlist_swap.h

---
+++ src/wishlist_swap.h
@@ -0,0 +1,13 @@
+#ifndef __WISHLIST_SWAP_H__
+#define __WISHLIST_SWAP_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+#include "listdin.h"
+#include "list.h"
+#include "linkedlist.h"
+
+void WishlistSwap(LinkedList *wishlist, int i, int j);
+
+#endif

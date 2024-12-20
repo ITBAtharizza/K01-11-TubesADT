2434ad0197f045a02a49d71d3f9a986a9b36eea1
041KeziaCarenCahyadi <18222041@std.stei.itb.ac.id>
2024-12-20 09:24:15 +0700

    Update wishlist_clear.c

--- src/wishlist_clear.c
+++ src/wishlist_clear.c
@@ -2,6 +2,10 @@
 #include "linkedlist.h"
 
 void ClearWishlist(LinkedList *wishlist) {
+    if (IsEmptyLinkedList(*wishlist)) {
+        printf("Wishlist sudah kosong.\n");
+        return;
+    }
     infotypeLL temp;
     while (!IsEmptyLinkedList(*wishlist)) {
         DelVFirstLinkedList(wishlist, &temp);
